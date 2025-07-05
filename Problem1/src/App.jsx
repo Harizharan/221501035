c1682e7828eb9bae65c567d4ec97b1de94df1521
Hariharan A <140934212+Harizharan@users.noreply.github.com>
2025-07-05 10:44:28 +0530

    init app

---
+++ Problem1/src/App.jsx
@@ -0,0 +1,11 @@
+import PokemonDisplay from "./components/PokemonDisplay";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <PokemonDisplay />
+    </>
+  );
+}
+
+export default App;
